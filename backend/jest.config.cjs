9a0d9b7d3218322bfa18ecde0f8b0d2c3eedff4c
ajaxz01 <azhar.ala99@gmail.com>
2025-11-22 02:38:22 +0700

    chore(test): add jest-stare reporter and configure coverage
    - Added `jest-stare` reporter for detailed test reports
    - Updated `jest.config.cjs` to include `jest-stare` and coverage reporters
    - Added `jest-stare/` to `.gitignore`
    - Installed `jest-stare` in `package.json` and updated lockfile

--- backend/jest.config.cjs
+++ backend/jest.config.cjs
@@ -8,7 +8,22 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'js', 'json'],
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
+  
+  // Coverage
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  coverageReporters: ['json', 'text', 'lcov'], // json = file, text = console, lcov = html
+  
+  // Roots
   roots: ['<rootDir>/src', '<rootDir>/test'],
+
+  // Reporters
+  reporters: [
+    "default",
+    ["jest-stare", {
+      "resultDir": "jest-stare",
+      "reportTitle": "Test Report",
+      "additionalResultsProcessors": []
+    }]
+  ],
 };
